176ef80e7e5f155b6833ee082738b73c564d47e4
aj <aj@glostars.net>
2024-01-06 02:45:00 -0500

    z

--- api/auth/discord/redirect.js
+++ api/auth/discord/redirect.js
@@ -5,6 +5,7 @@ import { MongoClient } from 'mongodb';
 const client = new MongoClient(process.env.MONGODB_URI);
 
 export default async (req, res) => {
+    let userInfo;
     const { code } = req.query;
 
     if (!code) {
