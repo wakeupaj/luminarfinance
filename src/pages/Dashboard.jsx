86146d82eddfaa744a1697af9f24da58bf8f17a5
wakeupaj <aj@glostars.net>
2024-01-12 02:57:20 -0500

    avatar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -23,22 +23,32 @@ const Dashboard = () => {
 
   return (
     <div>
-      <div className={`${theme === 'light' ? 'bg-light-background' : 'bg-background'} ${theme === 'light' ? 'text-light-text' : 'text-text'} ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>
-            <section id="dashboard" className={`flex md:flex-row flex-col ${styles.paddingY} ${theme === 'light' ? 'bg-light-background' : 'bg-background'}`}>
-                <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}> 
-                  <h1 className={`font-inter font-normal text-[44px] ${theme === 'light' ? 'text-light-text' : 'text-text'}`}>
-                    Hello, {username}.
-                  </h1>
-                  <img src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`} className={`w-[50px] h-[50px] rounded-full ml-4`} alt="avatar"/>
-              </div>
-                  <img src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`} className={`w-[50px] h-[50px] rounded-full`} alt="avatar"/>
-                  <p className={`font-inter font-normal text-[16px]`}>{email}</p>
-            </section>
-        </div>
+      <div className={`${
+        theme === 'light' ? 'bg-light-background' : 'bg-background'
+      } ${styles.boxWidth}`}>
+        <section id="dashboard" className={`flex md:flex-row flex-col ${styles.paddingY} ${
+          theme === 'light' ? 'bg-light-background' : 'bg-background'
+        }`}>
+          <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+            <div className="flex items-center justify-start">
+              <h1 className={`font-inter font-normal text-[44px] ${
+                theme === 'light' ? 'text-light-text' : 'text-text'
+              }`}>
+                Hello, {username}
+              </h1>
+              <img 
+                src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`} 
+                className="w-[50px] h-[50px] rounded-full ml-4" 
+                alt="avatar"
+              />
+            </div>
+            <p className={`font-inter font-normal text-[21px] ${theme === 'light' ? 'text-light-text' : 'text-text'}`}>
+              {email}
+            </p>
+          </div>
+        </section>
       </div>
     </div>
-  )
-}
+)};
 
 export default Dashboard
