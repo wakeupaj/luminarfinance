ff9388ea8452ba90d7afec90a0a0f662e6504fea
AJ <aj@glostars.net>
2024-01-12 02:08:12 -0500

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,11 +20,22 @@ const Dashboard = () => {
 
   return (
     <div>
-      <h1>Hello, {username}</h1>
-      <p>Your email is: {email}</p>
-      <p>
-        Your avatar is: <img src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`} className='avatar'/>
-      </p>
+      
+
+      <div className={`${theme === 'light' ? 'bg-light-background' : 'bg-background'} ${theme === 'light' ? 'text-light-text' : 'text-text'} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <section id="dashboard" className={`flex md:flex-row flex-col ${styles.paddingY} ${theme === 'light' ? 'bg-light-background' : 'bg-background'}`}>
+              <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
+                  <h1>Hello, {username}</h1>
+                  <p>Your email is: {email}</p>
+                  <p>
+                    Your avatar is: <img src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`} className='avatar'/>
+                  </p> 
+                </div>
+            </section>
+          </div>
+        </div>
 
     </div>
   )
