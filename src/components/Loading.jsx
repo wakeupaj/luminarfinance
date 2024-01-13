100f7bf1579f491ec3ed27fed4c41a93979923d9
AJ <aj@glostars.net>
2024-01-12 22:10:45 -0500

    loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactLoading from 'react-loading'
+import { useTheme } from '../context/ThemeContext.jsx';
 
-const Loading = ({ type, color }) => (
-	<ReactLoading type={type} color={color} height={'20%'} width={'20%'} />
-);
+
+const Loading = ({ type, color }) => {
+	
+	const { theme } = useTheme();
+
+	<ReactLoading type="bubbles" color={theme === 'light' ? 'text-light-text' : 'text-text'} height={'20%'} width={'20%'} />
+};
 
 export default Loading
